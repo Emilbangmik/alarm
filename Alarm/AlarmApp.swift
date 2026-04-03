d7e6c2ce528c1e08221902884a6bed9b2c1b3be0
ebm-jpg <ebm@resights.dk>
2026-04-03 15:47:26 +0200

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Emil Bang Mikkelsen on 03/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alarm
+//
+//  Created by Emil Bang Mikkelsen on 03/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
